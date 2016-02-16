34814cc2ca3b56dd80938d500a612488a22b53c4
emilti <emilti1986@gmail.com>
2016-02-16 15:44:56 +0200

    Additional validation attributes for Account added

--- Source/Web/SportsBook.Web/ViewModels/Account/LoginViewModel.cs
+++ Source/Web/SportsBook.Web/ViewModels/Account/LoginViewModel.cs
@@ -1,16 +1,23 @@
 ﻿namespace SportsBook.Web.ViewModels.Account
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     public class LoginViewModel
     {
         [Required]
+        [AllowHtml]
         [Display(Name = "Username")]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string UserName { get; set; }
 
         [Required]
+        [AllowHtml]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
