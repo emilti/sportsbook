4fe295465c8f260f532aa06bfeb521b050c7d4ad
emilti <emilti1986@gmail.com>
2016-02-16 15:50:44 +0200

    Validation for AccountDetailsViewModel added

--- Source/Web/SportsBook.Web/ViewModels/Account/AccountDetailsViewModel.cs
+++ Source/Web/SportsBook.Web/ViewModels/Account/AccountDetailsViewModel.cs
@@ -8,6 +8,7 @@
     using AutoMapper;
     using Data.Models;
     using Infrastructure.Mapping;
+    using System.Web.Mvc;
 
     public class AccountDetailsViewModel : IMapFrom<AppUser>, IHaveCustomMappings
     {
@@ -20,19 +21,34 @@
             this.Facilities = new HashSet<Facility>();
         }
 
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [AllowHtml]
+        [Display(Name = "Username")]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string UserName { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [AllowHtml]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
+        [Display(Name = "Avatar")]
         public string Avatar { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string LastName { get; set; }
 
