d7c37ddd6230963cfadc9764d369c16757cd826d
emilti <emilti1986@gmail.com>
2016-02-16 14:43:41 +0200

    Some validation added

--- Source/Data/SportsBook.Data.Models/AppUser.cs
+++ Source/Data/SportsBook.Data.Models/AppUser.cs
@@ -1,12 +1,13 @@
 ﻿namespace SportsBook.Data.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Security.Claims;
     using System.Threading.Tasks;
+    using Common.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
-    using System.Collections.Generic;
-    using Common.Models;
-    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class AppUser : IdentityUser
@@ -21,15 +22,21 @@
         }
 
         [Required]
+        [AllowHtml]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
 
         public string Avatar { get; set; }
 
         [Required]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string FirstName { get; set; }
 
         [Required]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string LastName { get; set; }
 
--- Source/Web/SportsBook.Web/ViewModels/Account/RegisterViewModel.cs
+++ Source/Web/SportsBook.Web/ViewModels/Account/RegisterViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace SportsBook.Web.ViewModels.Account
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     public class RegisterViewModel
     {
@@ -10,32 +11,44 @@
         public string Email { get; set; }
 
         [Required]
+        [AllowHtml]
         [Display(Name = "Username")]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string UserName { get; set; }
 
         [Required]
         [Display(Name = "First Name")]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string FirstName { get; set; }
 
         [Required]
         [Display(Name = "Last Name")]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string LastName { get; set; }
 
+        [AllowHtml]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
         [Display(Name = "Avatar")]
         public string Avatar { get; set; }
 
         [Required]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
+        [AllowHtml]
+        [RegularExpression(@"[a-zA-Z_0-9]+", ErrorMessage = "Only letters, numbers and _ allowed")]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
