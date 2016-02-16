4901208e424d74f23eeb2e78e17746b45ba5b150
emilti <emilti1986@gmail.com>
2016-02-16 17:01:38 +0200

    New validation added to the facility view models

--- Source/Web/SportsBook.Web/ViewModels/Facilities/FacilityDetailedViewModel.cs
+++ Source/Web/SportsBook.Web/ViewModels/Facilities/FacilityDetailedViewModel.cs
@@ -7,6 +7,8 @@
     using AutoMapper;
     using Data.Models;
     using SportsBook.Web.Infrastructure.Mapping;
+    using System.Web.Mvc;
+    using System.ComponentModel.DataAnnotations;
 
     public class FacilityDetailedViewModel : IMapFrom<SportsBook.Data.Models.Facility>, IHaveCustomMappings
     {
@@ -22,14 +24,23 @@
 
         public int Id { get; set; }
 
+        [AllowHtml]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string Name { get; set; }
 
+        [AllowHtml]
+        [StringLength(2000, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
         public string Description { get; set; }
 
         public int CityId { get; set; }
 
         public virtual City City { get; set; }
 
+        [Required]
+        [AllowHtml]
+        [RegularExpression(@"^[^<>]*$", ErrorMessage = "Invalid symbol")]
         public string Image { get; set; }
 
         public virtual ICollection<SportCategory> SportCategories
