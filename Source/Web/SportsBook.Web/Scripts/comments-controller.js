7406b17dd00d82188dd87363e2bfc724c3a9fd0b
emilti <emilti1986@gmail.com>
2017-03-03 12:20:11 +0200

    Showing all the latest comments added on top of the comment section in facility details page

--- Source/Web/SportsBook.Web/Scripts/comments-controller.js
+++ Source/Web/SportsBook.Web/Scripts/comments-controller.js
@@ -14,15 +14,10 @@ $(".submit-comment-button").on("click", function (event) {
        { id: facilityId, content: content }, function success(data, textStatus, jqXHR) {          
            var url = '@Url.Action("GetLatestComment", "Comments", new { area= "Facilities", id="facilityId" });';
            url = url.replace("facilityId", facilityId);
-           //foo(url);
-           //HTML element to load the partial view 
            history.pushState({}, null, "/Facilities/FacilitiesPublic/");
-           $("#latest-comment-container").load("RedirectToLatestGetCommentAction/" + facilityId);
-           
+           $("#latest-comments-container").prepend("<div></div>");
+           var addedRecentlyComments = $("#latest-comments-container").children();
+           $(addedRecentlyComments[0]).load("RedirectToLatestGetCommentAction/" + facilityId);
+           history.pushState({}, null, "/Facilities/FacilitiesPublic/FacilityDetails/" + facilityId);
        });
 })
-
-function foo(link) {
-    window.location.href = link;
-}
-// });
