003902bf517b78fd22caaa27e8972737cfbfb101
emilti <emilti1986@gmail.com>
2017-03-10 22:54:15 +0200

    Controlling dialog for login to appear when user is not registered and clicked to add to favorites

---
+++ Source/Web/SportsBook.Web/Scripts/dialog-controller.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $("#dialog-form").dialog({
+        autoOpen: false,
+        width: 200,
+        height: 400,
+        modal: true,
+        dialogClass: 'dialog-title'
+    })
+});
+
