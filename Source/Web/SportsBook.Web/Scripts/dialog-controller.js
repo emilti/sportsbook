65decb48e5d79d96b5ebbbf7ce8444769358bc1a
emilti <emilti1986@gmail.com>
2017-03-11 18:34:38 +0200

    Functionality for Login from popup window added using jquery ajax call

--- Source/Web/SportsBook.Web/Scripts/dialog-controller.js
+++ Source/Web/SportsBook.Web/Scripts/dialog-controller.js
@@ -1,11 +1,21 @@
 ﻿$(function () {
-    $("#dialog-form").dialog({
+    var dialog = $("#dialog-form").dialog({
         autoOpen: false,
         width: 200,
         height: 300,
         modal: true,
-        dialogClass: 'dialog-title'
+        dialogClass: 'dialog-title'        
     })
     $(".ui-dialog-titlebar").hide()
 });
 
+$(document).on("click", "#login-form .login-button", function (e) {
+    var username = $("#login-form .username").val(),
+    password = $("#login-form .password").val();
+    var loginModel = { UserName: username, Password: password, __RequestVerificationToken: gettoken() };
+    $.post("/Account/Login",
+       loginModel, function success(data, textStatus, jqXHR) {
+           $("#dialog-form").dialog("close");        
+           location.reload();
+       });
+})
